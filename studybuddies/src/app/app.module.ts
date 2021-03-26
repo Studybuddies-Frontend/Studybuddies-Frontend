cf18d53c0367ca8e1fff1674fa55d72df06d4c0e
Álvaro Díaz-Ligüeri Ascaso <alvdiaasc@alum.us.es>
2021-03-26 17:01:07 +0100

    Modificación esqueleto aplicación

--- studybuddies/src/app/app.module.ts
+++ studybuddies/src/app/app.module.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { StudentComponent } from './student/student.component';
-import { TutorComponent } from './tutor/tutor.component';
-import { ClassListStudentComponent } from './student/class-list-student/class-list-student.component';
-import { CreateClassStudentComponent } from './student/create-class-student/create-class-student.component';
-import { ClassListTutorComponent } from './tutor/class-list-tutor/class-list-tutor.component';
-import { CreateClassTutorComponent } from './tutor/create-class-tutor/create-class-tutor.component';
-import { HomeComponent } from './home/home.component';
+import { StudentComponent } from './components/student/student.component';
+import { TutorComponent } from './components/tutor/tutor.component';
+import { ClassListStudentComponent } from './components/student/class-list-student/class-list-student.component';
+import { CreateClassStudentComponent } from './components/student/create-class-student/create-class-student.component';
+import { ClassListTutorComponent } from './components/tutor/class-list-tutor/class-list-tutor.component';
+import { CreateClassTutorComponent } from './components/tutor/create-class-tutor/create-class-tutor.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: 'student', component: StudentComponent },
