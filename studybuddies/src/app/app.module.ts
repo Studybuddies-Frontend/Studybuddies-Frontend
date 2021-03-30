4771c4a17f564a2f4ddf5cd3993fec7a0d1726bd
Marta <marperoli2@alum.us.es>
2021-03-30 18:25:54 +0200

    Arreglando errores en los tests

--- studybuddies/src/app/app.module.ts
+++ studybuddies/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -55,9 +57,10 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    FormsModule
+    FormsModule,
+    
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
