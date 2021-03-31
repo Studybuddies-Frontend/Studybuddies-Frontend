11e615fdad5c418b1d387a88560d41af12c2da47
Santiago <santimartin3105@gmail.com>
2021-04-01 00:23:47 +0200

    Prueba conexión login con backend

--- studybuddies/src/app/services/auth.service.ts
+++ studybuddies/src/app/services/auth.service.ts
@@ -1,24 +1,92 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { UsuarioModel } from '../models/usuario.model';
+
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private URL = 'http://localhost:3000/api'
+  private url = 'http://localhost:3000/api/v1/user'; 
 
-  constructor(private http: HttpClient) { }
+  userToken: string;
 
-  signUp(user: any){
-    return this.http.post<any>(this.URL + '/user/register', user);
+
+
+  constructor( private http: HttpClient ) {
+    this.leerToken();
   }
 
-  signIn(user: any){
-    return this.http.post<any>(this.URL + '/user/login', user);
+
+  logout() {
+    localStorage.removeItem('token');
   }
 
-  loggedIn() {
-    return !!localStorage.getItem('token');
+  login( usuario: UsuarioModel ) {
+
+    const authData = {
+      ...usuario,
+      returnSecureToken: true
+    };
+
+    return this.http.post(
+      `${ this.url }/login`,
+      authData
+    ).pipe(
+      map( resp => {
+        this.guardarToken( resp['idToken'] );
+        return resp;
+      })
+    );
+
   }
+
+  private guardarToken( idToken: string ) {
+
+    this.userToken = idToken;
+    localStorage.setItem('token', idToken);
+
+    let hoy = new Date();
+    hoy.setSeconds( 3600 );
+
+    localStorage.setItem('expira', hoy.getTime().toString() );
+
+
+  }
+
+  leerToken() {
+
+    if ( localStorage.getItem('token') ) {
+      this.userToken = localStorage.getItem('token');
+    } else {
+      this.userToken = '';
+    }
+
+    return this.userToken;
+
+  }
+
+
+  estaAutenticado(): boolean {
+
+    if ( this.userToken.length < 2 ) {
+      return false;
+    }
+
+    const expira = Number(localStorage.getItem('expira'));
+    const expiraDate = new Date();
+    expiraDate.setTime(expira);
+
+    if ( expiraDate > new Date() ) {
+      return true;
+    } else {
+      return false;
+    }
+
+
+  }
+
+
 }
