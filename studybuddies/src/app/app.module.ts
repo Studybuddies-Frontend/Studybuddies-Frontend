081b3b6d78a9ca031243ee5223453a86abd7427e
Álvaro Díaz-Ligüeri Ascaso <alvdiaasc@alum.us.es>
2021-04-03 14:28:10 +0200

    Update app.module.ts

--- studybuddies/src/app/app.module.ts
+++ studybuddies/src/app/app.module.ts
@@ -3,9 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-
-import { HttpClientModule } from '@angular/common/http';
-
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
