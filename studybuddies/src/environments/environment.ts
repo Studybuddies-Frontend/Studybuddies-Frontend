984729d8413d30bc629f918be4d9a8825feb7040
Álvaro Díaz-Ligüeri Ascaso <alvdiaasc@alum.us.es>
2021-04-03 17:36:29 +0200

    Update environment.ts

--- studybuddies/src/environments/environment.ts
+++ studybuddies/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   name: 'desarrollo',
   urlBackend: 'http://localhost:3000/api/v1',
   rutaLogin: '/user/login',
-  rutaSalas: '/room'
+  rutaSalas: '/room',
   baseURL: 'http://localhost:1739'
 };
 
