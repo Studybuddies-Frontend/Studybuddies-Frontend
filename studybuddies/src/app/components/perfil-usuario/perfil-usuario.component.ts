2a858ebe291942326c197c9aee3256bd41903c71
Marta <marperoli2@alum.us.es>
2021-04-14 20:36:52 +0200

    Funciones para leer del back

--- studybuddies/src/app/components/perfil-usuario/perfil-usuario.component.ts
+++ studybuddies/src/app/components/perfil-usuario/perfil-usuario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/services/user.service'
 
 @Component({
   selector: 'app-perfil-usuario',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil-usuario.component.css']
 })
 export class PerfilUsuarioComponent implements OnInit {
+  guid!: string;
+  actualUser!: any;
 
-  constructor() { }
+
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService) { }
 
   ngOnInit(): void {
+  this.guid = this.route.snapshot.params['guid']
+  this.getUserByGuid();
   }
+
+  private getUserByGuid() {
+    this.userService.getUserByGuid(this.guid)
+      .subscribe((res: any) => {
+        this.actualUser = res.user[0];
+        console.log(this.actualUser)
+      })
+  }
+
+  
 
 }
--- studybuddies/src/app/services/user.service.ts
+++ studybuddies/src/app/services/user.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private urlUser = '/user';
+  private httpHeaders: HttpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+
+  constructor(private http: HttpClient) { 
+    this.urlUser = `${environment.urlBackend}/user`
+  }
+
+  getUserByGuid(guid:any): Observable<any> {
+    let url = this.urlUser + `/${guid}`
+    return this.http.get(url, { headers: this.httpHeaders })
+  }
 }
