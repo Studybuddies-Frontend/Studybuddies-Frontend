a01b77a6c804d932f4bf8ed9d2d617a642ea7c33
Álvaro Díaz-Ligüeri Ascaso <alvdiaasc@alum.us.es>
2021-04-18 17:19:03 +0200

    Pago terminado

--- studybuddies/src/app/components/tutor/view-class-tutor/view-class-tutor.component.ts
+++ studybuddies/src/app/components/tutor/view-class-tutor/view-class-tutor.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SalasService } from 'src/app/services/salas.service';
 import { TokenStorageService } from '../../../services/token-storage.service';
+import { AuthService } from "src/app/services/auth.service";
+
 
 @Component({
   selector: 'app-view-class-tutor',
@@ -12,15 +14,26 @@ export class ViewClassTutorComponent implements OnInit {
 
   guid!: string;
   actualRoom!: any;
+  authorised_users: string[];
+  id_user_app: any;
+  paid = false;
 
   constructor(
     private route: ActivatedRoute,
     private roomService: SalasService,
-    public tokenStorage: TokenStorageService) { }
+    public tokenStorage: TokenStorageService, public auth: AuthService) {
+  }
+
+
+  
 
   ngOnInit(): void {
+    
     this.guid = this.route.snapshot.params['guid']
     this.getRoomByGuid();
+    this.getAuthUsers();
+    this.id_user_app = this.auth.getId();
+    
   }
 
   private getRoomByGuid() {
@@ -31,7 +44,24 @@ export class ViewClassTutorComponent implements OnInit {
       })
   }
 
-  saveData(){
+  private getAuthUsers() {
+    this.roomService.getRoomByGuid(this.guid)
+      .subscribe((res: any) => {
+        this.authorised_users = res.room[0].authorised_users;
+        if(this.authorised_users.includes(this.id_user_app)){
+          this.paid = true;
+        }
+      })
+    return this.authorised_users
+
+  }
+  reloadPage(): void {
+    window.location.reload()
+  }
+
+  saveData() {
+    this.reloadPage();
     return this.tokenStorage.saveRoom(this.guid, this.actualRoom['price_per_hour']);
   }
+
 }
