c714303acd85cca5a9ed53494b4c14c6be45c120
= <=>
2021-04-24 19:57:32 +0200

    Añado el listado de tutores

--- studybuddies/src/app/app.module.ts
+++ studybuddies/src/app/app.module.ts
@@ -45,6 +45,7 @@ import { MytutoriasListStudentComponent } from './components/student/mytutorias-
 import { ShowTutorComponent } from './components/tutor/show-tutor/show-tutor.component';
 import { CommonModule } from '@angular/common';
 import { PerfilUsuarioComponent } from './components/perfil-usuario/perfil-usuario.component';
+import { ListTutorComponent } from './components/tutor/list-tutor/list-tutor.component';
 
 
 const routes: Routes = [
@@ -119,6 +120,7 @@ const routes: Routes = [
     }
   },
   { path: 'tutor/show/:id', component: ShowTutorComponent },
+  { path: 'tutores', component: ListTutorComponent },
   { path: 'tutor/classList', component: ClassListTutorComponent, canActivate: [AuthGuardService] },
   { path: 'tutor/classView/:guid', component: ViewClassTutorComponent, canActivate: [AuthGuardService] },
   { path: '', component: HomeComponent },
@@ -150,7 +152,8 @@ const routes: Routes = [
     MyclassesListStudentComponent,
     MyclassesListTutorComponent,
     MytutoriasListStudentComponent,
-    PerfilUsuarioComponent
+    PerfilUsuarioComponent,
+    ListTutorComponent
   ],
   imports: [
     BrowserModule,
